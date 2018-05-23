77dbc4461fede8e61343578880cbf764e06fb198
didi <shaoxuezheng_i@didichuxing.com>
2018-05-23 19:03:49 +0800

    update

--- server/models/student.js
+++ server/models/student.js
@@ -5,43 +5,41 @@ var async = require('async');
 mongoose.connect('mongodb://localhost/testApp');
 
 const StudentSchema = new Schema({
-  _id: String,
   name: {
     type: String,
     required: true
   },
   age: Number
-},{discriminatorKey: 'kind',_id: false});
+},{discriminatorKey: 'kind'});
 
 const Student = mongoose.model('Student', StudentSchema);
-const LocalSchema = new Schema({ code: String },{_id: false});
-
-/*/!*子表字段和通用表字段优先级*!/
-console.log(LocalSchema.path('_id').instance);
-/!*子表字段和通用表字段优先级*!/*/
+const LocalSchema = new Schema({ code: String });
+const ForeignSchema = new Schema({ salary: Number});
 
 const LocalStudent = Student.discriminator('LocalStudent', LocalSchema);
-const ForeignStudent = Student.discriminator('ForeignStudent', new Schema({
-  salary: Number,
-}));
+const ForeignStudent = Student.discriminator('ForeignStudent', ForeignSchema);
 
 const student1 = new Student({name:'test1',age:1});
-const ergou = new LocalStudent({_id:'ergou',name: 'ergou', age: 1, code: 'M123'});
-
-/*子表字段和通用表字段优先级*/
-console.log(typeof ergou._id);
-/*子表字段和通用表字段优先级*/
+const ergou = new LocalStudent({name: 'ergou', age: 1, code: 'M123'});
 
 const tony = new ForeignStudent({name: 'tony', age: 1, salary: 100});
+
+Student.find({}, async (err, docs)=>{
+  docs.map((doc)=>{
+    doc.remove();
+  });
+});
 
 async.map([student1, ergou, tony], (item, callback) => {
   item.save((err, doc)=>{
     callback(null, doc);
   })
-},(err, docs)=>{
-  Student.count({},(err=null,count)=>{
+},(err)=>{
+  Student.count({},(err,count)=>{
     console.log(count);
+  });
+
+  Student.find({}, (err, person)=> {
+    console.log(person);
   })
 });
-
-
