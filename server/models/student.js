9b317dc8a653c6c20782625c2a031dc7230a0338
miracle9312 <m18202787834@163.com>
2018-05-25 20:14:54 +0800

    update

--- server/models/student.js
+++ server/models/student.js
@@ -1,3 +1,4 @@
+'======================discriminator======================================'
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var async = require('async');
@@ -9,7 +10,11 @@ const StudentSchema = new Schema({
     type: String,
     required: true
   },
-  age: Number
+  age: Number,
+  subject: {
+    type: Schema.Types.ObjectId,
+    ref: 'Subject'
+  }
 },{discriminatorKey: 'kind'});
 
 const Student = mongoose.model('Student', StudentSchema);
@@ -21,27 +26,66 @@ const ForeignStudent = Student.discriminator('ForeignStudent', ForeignSchema);
 
 const student1 = new Student({name:'test1',age:1});
 const ergou = new LocalStudent({name: 'ergou', age: 1, code: 'M123'});
-
 const tony = new ForeignStudent({name: 'tony', age: 1, salary: 100});
 
+/*Student.remove({}, (err, docs)=>{
+  async.map([student1, ergou, tony], (item, callback) => {
+    item.save((err, doc)=>{
+      callback(null, doc);
+    })
+  },(err, docs)=>{
+    Student.count({},(err,count)=>{
+      console.log('============discriminator==============',count);
+    });
+
+    Student.find({},(err, docs) => {
+      console.log(docs);
+    })
+  });
+});*/
 
 
-Student.find({}, async (err, docs)=>{
-  docs.map((doc)=>{
-    doc.remove();
+'======================populate======================================'
+
+
+const SubjectSchema = new Schema({
+  name: String,
+  time: Number,
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: 'Student'
+  }
+});
+
+const Subject = mongoose.model('Subject', SubjectSchema);
+
+const tom = new Student({
+  name : 'tom',
+  age: 18,
+});
+
+tom.save((err, doc)=>{
+  Subject.remove({}, (err, docs)=>{
+    const math = new Subject({
+      name: 'math',
+      time: 38,
+      student: tom
+    });
+
+    math.save((err, doc) => {
+      if(err) return err;
+    });
   });
 });
 
-async.map([student1, ergou, tony], (item, callback) => {
-  item.save((err, doc)=>{
-    callback(null, doc);
-  })
-},(err)=>{
-  Student.count({},(err,count)=>{
-    console.log(count);
+Subject.find({},(err, docs)=>{
+  console.log(docs);
+});
+
+Subject.findOne({name: 'math'})
+  .populate('student')
+  .exec((err, doc) => {
+    console.log('========populate=======', doc.student);
   });
 
-  Student.find({}, (err, person)=> {
-    console.log(person);
-  })
-});
+Student.findOne()
